50ad9158ede72653131e6ad95ab586abbecdaaf9
Rahul <rahulscodingworld@gmail.com>
2022-04-18 10:12:52 -0400

    more bugs

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : process.env.DB_HOST,
       port : 5432,
       user : '',
       password : '',
