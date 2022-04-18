0cabd0d1bacae74a0410fc63106a7f1b6b99a15b
Rahul <rahulscodingworld@gmail.com>
2022-04-18 13:49:14 -0400

    fix

--- server.js
+++ server.js
@@ -11,11 +11,8 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : "postgresql-symmetrical-09707",
-      user : 'rahulrajkumar',
-      // port: 5432,
-      password : '',
-      database : 'smart-brain'
+      connectionString : process.env.DATABASE_URL, 
+      ssl: true
     }
   });
 
