ff51137efb835588040cd05bec735a1055ea4e1d
Rahul <rahulscodingworld@gmail.com>
2022-04-18 10:21:51 -0400

    more bugs

--- server.js
+++ server.js
@@ -11,8 +11,8 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : process.env.DB_HOST,
-      user : '',
+      host : '127.0.0.1',
+      user : 'rahulrajkumar',
       password : '',
       database : 'smart-brain'
     }
