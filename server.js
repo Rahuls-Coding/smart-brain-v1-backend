e4b5f94abd9c80fa27b57b6f9685b9f40c0d1770
Rahul <rahulscodingworld@gmail.com>
2022-04-18 13:27:09 -0400

    bug fix

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const image = require('./controllers/image');
 const db = knex ({
     client: 'pg',
     connection: {
-      host : '127.0.0.1',
+      host : process.env.DATABASE_URL,
       user : 'rahulrajkumar',
       password : '',
       database : 'smart-brain'
