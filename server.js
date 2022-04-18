73a432405fa9ee6eac78fba7972548d22bdc71ea
Rahul <rahulscodingworld@gmail.com>
2022-04-18 10:17:53 -0400

    more bugs

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ const db = knex ({
     client: 'pg',
     connection: {
       host : process.env.DB_HOST,
-      port : 5432,
       user : '',
       password : '',
       database : 'smart-brain'
